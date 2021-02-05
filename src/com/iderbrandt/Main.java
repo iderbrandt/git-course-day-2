3511ebde9b2e1af12dcce3e6094c9ad453abb6f0
iderbrandt <tobias.iderbrandt@hm.com>
2021-02-05 09:37:54 +0100

    small change on row 6

--- src/com/iderbrandt/Main.java
+++ src/com/iderbrandt/Main.java
@@ -3,7 +3,7 @@ package com.iderbrandt;
 public class Main {
 
     public static void main(String[] args) {
-	int num = 10;
+	int num = 15;
 	int increment = increment(num);
 
         System.out.println(increment);
